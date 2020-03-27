942e10c60fb7bb53b4be5982c23623b3e0a26000
JasonDeras <derasjason2@gmail.com>
2020-03-27 12:52:56 -0600

    Cracion de la Tupla

---
+++ Tupla.cpp
@@ -0,0 +1,60 @@
+#include<iostream> 
+#include<string> 
+#include<vector>
+#include<bits/stdc++.h>
+
+#ifndef TUPLA_CPP
+#define TUPLA_CPP
+using namespace std;
+
+class Tupla{
+      
+      private:
+              int id;
+			  vector<string>atributos;                        
+      public:
+
+            Tupla(){
+                id=0;
+            }//constructor vacio
+            
+            Tupla(int id){
+                this->id=id;        
+             } 
+                         
+             void setId(int id){
+                this->id=id;  
+             }//set del id;
+
+             int getId(){
+                return id;
+             }//get del id
+
+            void setAtributos(vector<string>atributos){
+                this->atributos=atributos;
+            }//Set de los atributos
+             
+            void setAtributo(string atributo){
+                this->atributos.push_back(atributo);
+            }//Set de una solo atributo
+             
+            vector<string> getAtributos(){
+                return this->atributos;
+            }//Get del vector de los atributos
+
+            void print(){
+
+                cout<<"La Tupla "<<endl 
+                "id:"<<id<<endl;
+
+                cout<<"Hobbies"<<endl;
+                for(int i=0;i<atributos.size();i++){
+                  	atributos.at(i)->print();
+				}//fin del for de impresion de los atributos
+             }
+			 
+			~Tupla(){
+
+            }//destructor           
+};
+#endif
